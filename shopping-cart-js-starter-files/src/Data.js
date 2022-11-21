a1c0d325d0d09b0d14c0c6e3a631dfbb59510795
Mohammad <mohamadtohidi10@gmail.com>
2022-11-20 19:12:23 -0800

    final

---
+++ shopping-cart-js-starter-files/src/Data.js
@@ -0,0 +1,26 @@
+let shopItemsData = [
+  {
+    id: "jfhgbvnscs",
+    name: "Bags",
+    price: 50,
+    img: "images/img-1.jpg",
+  },
+  {
+    id: "ioytrhndcv",
+    name: "Shoes",
+    price: 80,
+    img: "images/img-2.png",
+  },
+  {
+    id: "wuefbncxbsn",
+    name: "T Shirt",
+    price: 100,
+    img: "images/img-3.png",
+  },
+  {
+    id: "thyfhcbcv",
+    name: "Suit",
+    price: 200,
+    img: "images/img-4.jpg",
+  },
+];
